10583c79fb2f010f5b0eb83305eae6880a60eb6c
KoiCrystal <jinyuyuyu@tutanota.com>
2024-01-03 18:20:53 +0800

    Update exercise1_16.rkt

--- exercise1_16.rkt
+++ exercise1_16.rkt
@@ -2,6 +2,14 @@
 (define (even? x)
   (= (remainder x 2) 0))
 
+#|
+for example, b^10 = (b^2)^5 = (b^2)^4 * (b^2)
+the part (b^2)^4 will go into next iteration
+but the remain part *(b^2) will make the process to be recursion,
+it will wait for the result of (b^2)^4
+so we use 'a' to save the "odd part" in several state that n is odd.
+|#
+
 
 (define (fast-expt-iter b n a)
   (if (= n 1)
