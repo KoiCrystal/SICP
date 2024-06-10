5a0ac942df9842a86bc84f03b81877fa3c0a45b8
KoiCrystal <jinyuyuyu@tutanota.com>
2024-06-10 20:52:25 +0800

    Create exercise1_23.rkt

---
+++ exercise1_23.rkt
@@ -0,0 +1,51 @@
+#lang sicp
+(define (small-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divisor? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (next n)
+  (if (= n 2) 3 (+ n 2)))
+
+(define (divisor? test-divisor n)
+  (= (remainder n test-divisor) 0))
+
+(define (square x) (* x x))
+
+(define (prime? n)
+  (= n (small-divisor n)))
+
+
+(define (timed-prime-test n amount)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime) amount))
+
+(define (start-prime-test n start-time amount)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time) n amount)
+      (timed-prime-test (+ n 1) amount)))
+
+(define (report-prime elapsed-time n amount)
+  (display "***")
+  (display elapsed-time)
+  (if (> amount 0)
+      (timed-prime-test (+ n 1) (- amount 1))))
+  
+(timed-prime-test 1000000 2)
+
+
+
+
+
+
+
+;https://stackoverflow.com/questions/2195105/is-there-an-equivalent-to-lisps-runtime-primitive-in-scheme
+;Open the Package Manager: in DrRacket choose the menu File then choose Package Manager....
+;In the tab Do What I Mean find the text field and enter: sicp
+;Finally click the Install button.
+
+;#lang sicp
+;(runtime)
