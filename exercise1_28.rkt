de81f4689b3ba6161a86f82cce11cd3480d51d86
KoiCrystal <jinyuyuyu@tutanota.com>
2024-07-25 14:03:43 +0800

    Create exercise1_28.rkt

---
+++ exercise1_28.rkt
@@ -0,0 +1,26 @@
+#lang scheme
+(define (square x)
+  (* x x))
+; use expmod many times, maybe I should use an argument to save the rusult of expmod recursion
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) (if (and (= (remainder (square (expmod base (/ exp 2) m)) m) 1)
+                              (not (= (expmod base (/ exp 2) m) 1))
+                              (not (= (expmod base (/ exp 2) m) (- m 1))))
+                         0
+                         (remainder (square (expmod base (/ exp 2) m)) m)
+                      ))
+        (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (try-it (+ 1 (random (- n 1)))))
+
+
+(define (fast-prime? n times)
+  (cond ((= times n) true)
+  ((fermat-test n) (fast-prime? n (+ times 1)))
+  (else false)))
+
+(fast-prime? 6601 1)
