c80bdd63b2562e317f9c2ebd82a9c19580835676
KoiCrystal <koicrystalkoi@icloud.com>
2024-09-23 16:45:13 +0800

    Create exercise_1_40.rkt

---
+++ exercise_1_40.rkt
@@ -0,0 +1,25 @@
+#lang scheme
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (if (close-enough? guess (f guess))
+        (f guess)
+        (try (f guess))))
+  (try first-guess))
+
+(define dx 0.00001)
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+  (lambda (x) (+ (* x x x) (* a x x) (* b x) c)))
+(define (zero-of-cubic a b c)
+  (newtons-method (cubic a b c) 1))
+(zero-of-cubic 1 2 3)
