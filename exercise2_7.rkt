232ecabeb60238ad3c1948160ba62eba5dc2607e
KoiCrystal <koicrystalkoi@icloud.com>
2024-10-08 21:32:59 +0800

    Update exercise2_7.rkt

--- exercise2_7.rkt
+++ exercise2_7.rkt
@@ -1,6 +1,6 @@
 #lang scheme
 (define (make-interval x y)
   (cons x y))
-(define (upper-bound interval) (car interval))
-(define (lower-bound interval) (cdr interval))
+(define (lower-bound interval) (car interval))
+(define (upper-bound interval) (cdr interval))
 
