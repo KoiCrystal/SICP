a352981ad795f407e6629fc3d88c3b554d99840e
KoiCrystal <koicrystalkoi@icloud.com>
2025-05-15 20:23:51 +0800

    Create exercise2_35.rkt

---
+++ exercise2_35.rkt
@@ -0,0 +1,22 @@
+#lang scheme
+
+; sequence is the list of num of leaves of every node
+; sequence of ((1 2) 3 4) is (2 1 1)
+; accumulate adds every number in sequence
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+; lambda (sub-tree) caculates num of leaves of every node
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (sub-tree) (cond ((not (pair? sub-tree)) 1)
+                                                (else (count-leaves sub-tree)))) 
+                        t)))
+(define x (cons (list 1 2) (list 3 4)))
+(define y (list x x))
+(count-leaves x)
+(count-leaves y)
+
+
